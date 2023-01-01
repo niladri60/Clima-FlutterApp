b5ab7c970021cd958308d6cc81901a86e325318d
Niladri <niladri.nandy2names@gmail.com>
2023-01-01 22:53:46 +0530

    V 0.3

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: true,
       theme: ThemeData.dark(),
-      home: LoadingScreen(),
+      home: const LoadingScreen(),
     );
   }
 }
