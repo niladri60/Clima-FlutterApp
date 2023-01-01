08fc6983ad0e105920d8654ed2122aa07caf6e2e
Niladri <niladri.nandy2names@gmail.com>
2023-01-01 16:15:10 +0530

    V 0.0

---
+++ lib/screens/loading_screen.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+
+class LoadingScreen extends StatefulWidget {
+  const LoadingScreen({super.key});
+
+  @override
+  LoadingScreenState createState() => LoadingScreenState();
+}
+
+class LoadingScreenState extends State<LoadingScreen> {
+  Future<void> getLocation() async {
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.low);
+    if (kDebugMode) {
+      print(position);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // backgroundColor: Colors.white,
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            getLocation();
+          },
+          child: const Text('Get Location'),
+        ),
+      ),
+    );
+  }
+}
