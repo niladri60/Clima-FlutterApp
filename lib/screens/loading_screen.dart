e3460767f2fd41e934bdcb61641731fe74b366ec
Niladri <niladri.nandy2names@gmail.com>
2023-01-02 20:27:52 +0530

    V 0.6

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart';
+import 'package:http/http.dart';
+
 class LoadingScreen extends StatefulWidget {
   const LoadingScreen({super.key});
 
@@ -21,8 +23,20 @@ class LoadingScreenState extends State<LoadingScreen> {
     print(location.longitude);
   }
 
+  void getData() async {
+    Response response = await get(Uri.parse(
+        'https://api.openweathermap.org/data/2.5/weather?lat=23.26&lon=88.43&appid=a2b8ed23b446401728b0ea4960b0e00e'));
+    if (response.statusCode == 200) {
+      String data = response.body;
+      print(data);
+    } else {
+      print(response.statusCode);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    getData();
     return const Scaffold(
       backgroundColor: Colors.white,
       body: Center(
